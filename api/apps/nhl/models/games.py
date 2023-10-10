f865d00bbf1bc42cd13a84b809b51aff0c420077
Daniel <danhamilton@efit.institute>
2023-10-09 22:16:19 -0400

    fixing games model

--- api/apps/nhl/models/games.py
+++ api/apps/nhl/models/games.py
@@ -6,7 +6,6 @@ from apps.nhl.constants import BASE_API_URL
 from apps.nhl.models.teams import Team, Franchise, Conference, Division, Venue
 
 class Freq(models.Model):
-    game = models.OneToOneField('nbaway_games.Game', to_field='game_id', related_name='freq')
     away_days_rest = models.IntegerField()
     away_pst_2_mst_b2b = models.BooleanField(db_column='amstb2b', default=False)
     away_first_of_2 = models.BooleanField(db_column='a1of2', default=False)
@@ -14,33 +13,19 @@ class Freq(models.Model):
     away_2_of_3 = models.BooleanField(db_column='a2of3', default=False)
     away_b2b = models.BooleanField(default=False, db_column='ab2b')
     away_b2b2b = models.BooleanField(default=False, db_column='ab2b2b')
-    # 2n3
     away_two_in_three = models.BooleanField(db_column='a2n3', default=False)
-    # 3n4
     away_three_in_four = models.BooleanField(db_column='a3n4', default=False)
-    # 4n5
     away_four_in_five = models.BooleanField(db_column='a4n5', default=False)
-    # 4n6
     away_four_in_six = models.BooleanField(db_column='a4n6', default=False)
-    # 5n6
     away_five_in_six = models.BooleanField(db_column='a5n6', default=False)
-    # 5n7
     away_five_in_seven = models.BooleanField(db_column='a5n7', default=False)
-    # 6n8
     away_six_in_eight = models.BooleanField(db_column='a6n8', default=False)
-    # 6n9
     away_six_in_nine = models.BooleanField(db_column='a6n9', default=False)
-    # 7n9
     away_seven_in_nine = models.BooleanField(db_column='a7n9', default=False)
-    # 7n10
     away_seven_in_ten = models.BooleanField(db_column='a7n10', default=False)
-    # 7n11
     away_seven_in_eleven = models.BooleanField(db_column='a7n11', default=False)
-    # 8n11
     away_eight_in_eleven = models.BooleanField(db_column='a8n11', default=False)
-    # 9n12
     away_nine_in_twelve = models.BooleanField(db_column='a9n12', default=False)
-
     home_days_rest = models.IntegerField()
     home_pst_2_mst_b2b = models.BooleanField(db_column='hmstb2b', default=False)
     home_first_of_2 = models.BooleanField(db_column='h1of2', default=False)
@@ -48,31 +33,18 @@ class Freq(models.Model):
     home_2_of_3 = models.BooleanField(db_column='h2of3', default=False)
     home_b2b = models.BooleanField(default=False, db_column='hb2b')
     home_b2b2b = models.BooleanField(default=False, db_column='hb2b2b')
-    # 2n3
     home_two_in_three = models.BooleanField(db_column='h2n3', default=False)
-    # 3n4
     home_three_in_four = models.BooleanField(db_column='h3n4', default=False)
-    # 4n5
     home_four_in_five = models.BooleanField(db_column='h4n5', default=False)
-    # 4n6
     home_four_in_six = models.BooleanField(db_column='h4n6', default=False)
-    # 5n6
     home_five_in_six = models.BooleanField(db_column='h5n6', default=False)
-    # 5n7
     home_five_in_seven = models.BooleanField(db_column='h5n7', default=False)
-    # 6n8
     home_six_in_eight = models.BooleanField(db_column='h6n8', default=False)
-    # 6n9
     home_six_in_nine = models.BooleanField(db_column='h6n9', default=False)
-    # 7n9
     home_seven_in_nine = models.BooleanField(db_column='h7n9', default=False)
-    # 7n10
     home_seven_in_ten = models.BooleanField(db_column='h7n10', default=False)
-    # 7n11
     home_seven_in_eleven = models.BooleanField(db_column='h7n11', default=False)
-    # 8n11
     home_eight_in_eleven = models.BooleanField(db_column='h8n11', default=False)
-    # 9n12
     home_nine_in_twelve = models.BooleanField(db_column='h9n12', default=False)
 
     def __str__(self):
@@ -85,9 +57,9 @@ class Game(models.Model):
     season = models.ForeignKey('Season', on_delete=models.CASCADE)
     game_date = models.DateTimeField()
     home_team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='home_team')
-    previous_home_game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='previous_home_game', null=True)
+    previous_home_game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='next_home_game', null=True)
     away_team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='away_team')
-    previous_away_game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='previous_away_game', null=True)
+    previous_away_game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='next_away_game', null=True)
     away_distance_travelled = models.FloatField(null=True)
     home_distance_travelled = models.FloatField(null=True)
     away_altitude_difference = models.FloatField(null=True)
@@ -95,7 +67,7 @@ class Game(models.Model):
     away_days_rest = models.IntegerField(null=True)
     home_days_rest = models.IntegerField(null=True) 
     freq = models.OneToOneField('Freq', on_delete=models.CASCADE, null=True)
-    venue = models.ForeignKey('Venue', on_delete=models.CASCADE)
+    venue = models.ForeignKey('Venue', on_delete=models.CASCADE, null=True)
     completed = models.BooleanField(default=False)
     live = models.BooleanField(default=False)
     class Meta:
@@ -104,13 +76,21 @@ class Game(models.Model):
     def __str__(self):
         return f"{self.game_id} - {self.game_date} - {self.home_team} vs {self.away_team}"
     
+    def calculate_freq(self):
+        pass
+
     def save(self, *args, **kwargs):
-        self.away_distance_travelled = haversine_distance(self.previous_away_game.venue.latitude, self.previous_away_game.venue.longitude, self.venue.latitude, self.venue.longitude)
-        self.home_distance_travelled = haversine_distance(self.previous_home_game.venue.latitude, self.previous_home_game.venue.longitude, self.venue.latitude, self.venue.longitude)
-        self.away_altitude_difference = self.previous_away_game.venue.altitude - self.venue.altitude
-        self.home_altitude_difference = self.previous_home_game.venue.altitude - self.venue.altitude
-        self.away_days_rest = (self.game_date - self.previous_away_game.game_date).days
-        self.home_days_rest = (self.game_date - self.previous_home_game.game_date).days
+        if self.previous_away_game:
+            self.away_altitude_difference = self.previous_away_game.venue.altitude - self.venue.altitude
+            self.away_days_rest = (self.game_date - self.previous_away_game.game_date).days
+        if self.previous_home_game:
+            self.home_altitude_difference = self.previous_home_game.venue.altitude - self.venue.altitude
+            self.home_days_rest = (self.game_date - self.previous_home_game.game_date).days
+        if self.previous_home_game and self.previous_away_game:
+            self.home_distance_travelled = haversine_distance(self.previous_home_game.venue.latitude, self.previous_home_game.venue.longitude, self.venue.latitude, self.venue.longitude)
+            self.away_distance_travelled = haversine_distance(self.previous_away_game.venue.latitude, self.previous_away_game.venue.longitude, self.venue.latitude, self.venue.longitude)
+
+        self.calculate_freq()
         super().save(*args, **kwargs)
     
 class Season(models.Model):
